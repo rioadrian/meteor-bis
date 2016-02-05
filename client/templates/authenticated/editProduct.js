fc4e67cd21c43a3d62d5dd4c598bef985cd5dcc7
Gusti Azis <gooazs@gmail.com>
2016-02-05 17:04:54 +0700

    Update Identaition Create Product and Edit Product Script

--- client/templates/authenticated/editProduct.js
+++ client/templates/authenticated/editProduct.js
@@ -1,25 +1,25 @@
 Template.editProduct.onRendered( () => {
-  autosize(document.querySelectorAll('.auto-size'));
-  changePhoto('.img-change-photo', '.btn-change-photo', '.input-change-photo');
+	autosize(document.querySelectorAll('.auto-size'));
+	changePhoto('.img-change-photo', '.btn-change-photo', '.input-change-photo');
 });
 
 function changePhoto(img, btn, input) {
 	$(btn).on('click', function(e) {
-    e.preventDefault();
-    $(input).click();
-  });
+		e.preventDefault();
+		$(input).click();
+	});
 
-  $(input).on('change', function() {
-    readURL(this, $(img)[0]);
-  });
+	$(input).on('change', function() {
+		readURL(this, $(img)[0]);
+	});
 }
 
 function readURL(input, img) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      img.src = e.target.result;
-    };
-    reader.readAsDataURL(input.files[0]);
-  }
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			img.src = e.target.result;
+		};
+		reader.readAsDataURL(input.files[0]);
+	}
 }
